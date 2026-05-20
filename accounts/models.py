43cc10ae07075e852a77c109ac0da680ea4568bd
Louremberg <kaualouremberg@gmail.com>
2026-05-20 14:34:23 -0300

    Alteração em account Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("E-mail obrigatório.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superusuário deve ter is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superusuário deve ter is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -9,6 +31,8 @@ class User(AbstractUser):
     avatar = models.FileField(upload_to="avatars/", blank=True)
     course = models.CharField(max_length=120, blank=True)
     semester = models.CharField(max_length=60, blank=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS: list[str] = []
